1dccc7c3f9e766b16d1a3abd6e20b0248cf2e192
DeveroCode <charli.rey.misterio@gmail.com>
2023-08-27 19:49:18 -0600

    Front End Files

---
+++ FrontEnd-MarketWide/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!-- <RouterView /> -->
+  <h1>Hello World</h1>
+</template>
+
+<style scoped></style>
