b0cd0fde7d2ad0983e35ebad4d5a30a636d79675
DeveroCode <charli.rey.misterio@gmail.com>
2023-08-30 20:08:18 -0600

    New: header view blocking

--- FrontEnd-MarketWide/src/App.vue
+++ FrontEnd-MarketWide/src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
 // Script vue
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
+import { computed } from 'vue';
+
+const router = useRoute();
+const auth = computed(() => {
+  return router.name === 'login' || router.name === 'register';
+});
 // Components
 import Header from './components/Header.vue';
 </script>
 
 <template>
   <!-- Header -->
-  <Header />
+  <Header v-if="!auth" />
   <RouterView />
 </template>
 
